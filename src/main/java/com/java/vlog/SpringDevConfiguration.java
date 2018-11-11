ffeea2d3d152b2d7164c3f2689006d4c02df1a37
jirka.pinkas@gmail.com <2DUYcOnWoHeLtjskqVBotRN2k>
2018-11-11 21:07:30 +0100

    cleanup - removed dbcp

---
+++ src/main/java/com/java/vlog/SpringDevConfiguration.java
@@ -0,0 +1,40 @@
+package com.java.vlog;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jndi.JndiTemplate;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Profile("dev")
+@Configuration
+public class SpringDevConfiguration {
+
+	@Bean
+	public DataSource dataSource() throws Exception {
+		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setJdbcUrl("jdbc:hsqldb:mem:test;sql.syntax_pgs=true");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+		LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+		entityManagerFactory.setDataSource(dataSource);
+		Properties jpaProperties = new Properties();
+		jpaProperties.put("hibernate.hbm2ddl.auto", "create");
+		jpaProperties.put("hibernate.show_sql", "true");
+		entityManagerFactory.setJpaProperties(jpaProperties);
+		entityManagerFactory.setPackagesToScan("com.java.vlog.entity");
+		entityManagerFactory.setPersistenceProvider(new HibernatePersistenceProvider());
+		return entityManagerFactory;
+	}
+
+}
